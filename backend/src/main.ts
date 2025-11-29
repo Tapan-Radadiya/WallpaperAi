076880e6babfa84690a6fc4cdc26c40aae68027a
Tapan Radadiya <radadiyatapan90@gmail.com>
2025-11-29 08:29:10 +0000

    (update) nestjs basic setup to run on IP

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3002;
+  await app.listen(port, '0.0.0.0');
+  console.log(`Server Running at port ${port} ✅`);
+  console.log(`Accessible at: http://192.168.1.31:${port}`);
+  console.log(`Local access: http://localhost:${port}`);
 }
 bootstrap();
