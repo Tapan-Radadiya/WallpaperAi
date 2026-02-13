2c2c9e4fddfab5e97fac15da7bfd58355554d3a4
Tapan Radadiya <radadiyatapan90@gmail.com>
2026-02-13 20:34:53 +0000

    (update) completed user reset password field flow

--- backend/src/user/schema/user_schema.ts
+++ backend/src/user/schema/user_schema.ts
@@ -54,6 +54,6 @@ export const tbl_user_reset_tickets = pgTable('tbl_user_reset_tickets', {
     id: uuid('id').defaultRandom().primaryKey(),
     userId: uuid('user_id').references(() => tbl_user.id).notNull(),
     userTicket: varchar('userTicket').notNull(),
-    expires_at: timestamp('expires_at').default(sql`NOW() + INTERVAL '5 minutes'`),
+    expires_at: timestamp('expires_at').default(sql`NOW() + INTERVAL '5 minutes'`).notNull(),
     created_at: timestamp('created_at').defaultNow(),
 })
